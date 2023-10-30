cb302f8a2db8048cfb9cb03d6cb7c264e6f0c921
Marco Di Modica Elias <marcodimodica@protonmail.com>
2023-10-30 15:42:19 +0100

    asd

---
+++ pinballbox2d - copia/ModuleSceneIntro.cpp
@@ -0,0 +1,79 @@
+#include "Globals.h"
+#include "Application.h"
+#include "ModuleRender.h"
+#include "ModuleSceneIntro.h"
+#include "ModuleInput.h"
+#include "ModuleTextures.h"
+#include "ModuleAudio.h"
+#include "ModulePhysics.h"
+
+ModuleSceneIntro::ModuleSceneIntro(Application* app, bool start_enabled) : Module(app, start_enabled)
+{
+	circle = box = rick = NULL;
+}
+
+ModuleSceneIntro::~ModuleSceneIntro()
+{}
+
+// Load assets
+bool ModuleSceneIntro::Start()
+{
+	LOG("Loading Intro assets");
+	bool ret = true;
+
+	App->renderer->camera.x = App->renderer->camera.y = 0;
+
+	circle = App->textures->Load("pinball/wheel.png"); 
+	//box = App->textures->Load("pinball/crate.png");
+	//rick = App->textures->Load("pinball/rick_head.png");
+	bonus_fx = App->audio->LoadFx("pinball/bonus.wav");
+
+	//sensor = App->physics->CreateRectangleSensor(SCREEN_WIDTH / 2, SCREEN_HEIGHT, SCREEN_WIDTH, 50);
+
+	return ret;
+}
+
+// Load assets
+bool ModuleSceneIntro::CleanUp()
+{
+	LOG("Unloading Intro scene");
+
+	return true;
+}
+
+// Update: draw background
+update_status ModuleSceneIntro::Update()
+{
+	if (App->input->GetKey(SDL_SCANCODE_A) == KEY_REPEAT)
+	{
+		App->physics->leftFlipperJoint->SetMotorSpeed(-10);
+	}
+	else
+	{
+		App->physics->leftFlipperJoint->SetMotorSpeed(10);
+	}
+
+	if (App->input->GetKey(SDL_SCANCODE_D) == KEY_REPEAT)
+	{
+		App->physics->rightFlipperJoint->SetMotorSpeed(10);
+	}
+	else
+	{
+		App->physics->rightFlipperJoint->SetMotorSpeed(-10);
+	}
+
+	if(App->input->GetKey(SDL_SCANCODE_1) == KEY_DOWN)
+	{
+		circles.add(App->physics->CreateCircle(App->input->GetMouseX(), App->input->GetMouseY(), 25));
+		circles.getLast()->data->listener = this;
+	}
+
+	return UPDATE_CONTINUE;
+}
+
+void ModuleSceneIntro::OnCollision(PhysBody* bodyA, PhysBody* bodyB)
+{
+	int x, y;
+
+	App->audio->PlayFx(bonus_fx);
+}
---
+++ pinballbox2d - copia/ModuleSceneIntro.h
@@ -0,0 +1,40 @@
+#ifndef __MODULESCENEINTRO_H__
+#define __MODULESCENEINTRO_H__
+
+#include "Module.h"
+#include "Globals.h"
+#include "p2Point.h"
+#include "ModulePhysics.h"
+
+#define DEGTORAD 0.0174532925199432957f
+
+struct PhysBody;
+struct PhysMotor;
+
+class ModuleSceneIntro : public Module
+{
+public:
+	ModuleSceneIntro(Application* app, bool start_enabled = true);
+	~ModuleSceneIntro();
+
+	bool Start();
+	update_status Update();
+	bool CleanUp();
+
+	void OnCollision(PhysBody* bodyA, PhysBody* bodyB);
+
+public:
+	PhysBody* sensor;
+	bool sensed;
+
+	SDL_Texture* circle;
+	SDL_Texture* box;
+	SDL_Texture* rick;
+	uint bonus_fx;
+
+	p2List<PhysBody*> circles;
+	p2List<PhysBody*> boxes;
+	p2List<PhysBody*> ricks;
+};
+
+#endif // __MODULESCENEINTRO_H__
