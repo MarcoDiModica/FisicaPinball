11b0172532d672e5321a497827b6884246258b45
PabloL2003 <pablol.seda@gmail.com>
2023-10-31 09:26:05 +0100

    Map

--- pinballbox2d - copia/Globals.h
+++ pinballbox2d - copia/Globals.h
@@ -21,8 +21,8 @@ enum update_status
 };
 
 // Configuration -----------
-#define SCREEN_WIDTH 600
-#define SCREEN_HEIGHT 900
+#define SCREEN_WIDTH 436
+#define SCREEN_HEIGHT 699
 #define SCREEN_SIZE 1
 #define WIN_FULLSCREEN false
 #define WIN_RESIZABLE false
